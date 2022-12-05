7a125b0f82105cec4af40d0a7a2c79e20bd140b3
Joseph Werle <joseph.werle@gmail.com>
2022-12-05 22:33:17 +0100

    refactor(android): use ipc bridge routing in scheme/postMessage

--- src/ipc/bridge.cc
+++ src/ipc/bridge.cc
@@ -1317,7 +1317,7 @@ static void registerSchemeHandler (Router *router) {
 #endif
 
 namespace SSC::IPC {
-  Bridge::Bridge (Core *core) {
+  Bridge::Bridge (Core *core) : router() {
     this->core = core;
     this->router.core = core;
     this->router.bridge = this;
@@ -1359,8 +1359,16 @@ namespace SSC::IPC {
   }
 
   bool Bridge::route (const String& msg, char *bytes, size_t size) {
+    return this->route(msg, bytes, size, nullptr);
+  }
+
+  bool Bridge::route (const String& msg, char *bytes, size_t size, Router::ResultCallback callback) {
     auto message = Message { msg, bytes, size };
-    return this->router.invoke(message);
+    if (callback != nullptr) {
+      return this->router.invoke(message, callback);
+    } else {
+      return this->router.invoke(message);
+    }
   }
 
   Router::Router () {
@@ -1440,6 +1448,7 @@ namespace SSC::IPC {
   }
 
   bool Router::invoke (const Message& message, ResultCallback callback) {
+    debug("invoke: %s", message.name.c_str());
     if (this->table.find(message.name) == this->table.end()) {
       return false;
     }
@@ -1447,6 +1456,7 @@ namespace SSC::IPC {
     auto ctx = this->table.at(message.name);
 
     if (ctx.callback != nullptr) {
+    debug("before dispatch");
       Message msg(message);
       // decorate message with buffer if buffer was previously
       // mapped with `ipc://buffer.map`, which we do on Linux
