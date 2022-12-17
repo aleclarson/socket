6d239b71b2b56aafb4ae2e28594bc539a69374bb
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-12-17 12:36:17 -0500

    feat: allow keyboard to appear without user interaction

--- src/mobile/ios.mm
+++ src/mobile/ios.mm
@@ -311,6 +311,7 @@ static dispatch_queue_t queue = dispatch_queue_create(
   [ns addObserver: self selector: @selector(keyboardWillChange:) name: UIKeyboardWillChangeFrameNotification object: nil];
 
   [self setUpWebView];
+  [self keyboardDisplayDoesNotRequireUserAction];
 
   return YES;
 }
@@ -475,6 +476,53 @@ static dispatch_queue_t queue = dispatch_queue_create(
   [self.window.rootViewController presentViewController:alert
                                                animated:YES
                                              completion:nil];
+}
+
+// https://github.com/Telerik-Verified-Plugins/WKWebView/commit/04e8296adeb61f289f9c698045c19b62d080c7e3#L609-L620
+- (void)keyboardDisplayDoesNotRequireUserAction {
+  Class contentViewClass = NSClassFromString(@"WKContentView");
+  NSOperatingSystemVersion iOS_11_3_0 = (NSOperatingSystemVersion){11, 3, 0};
+  NSOperatingSystemVersion iOS_12_2_0 = (NSOperatingSystemVersion){12, 2, 0};
+  NSOperatingSystemVersion iOS_13_0_0 = (NSOperatingSystemVersion){13, 0, 0};
+  char *methodSignature =
+      (char *)"_startAssistingNode:userIsInteracting:blurPreviousNode:"
+              "changingActivityState:userObject:";
+
+  if ([[NSProcessInfo processInfo]
+          isOperatingSystemAtLeastVersion:iOS_13_0_0]) {
+    methodSignature =
+        (char *)"_elementDidFocus:userIsInteracting:blurPreviousNode:"
+                "activityStateChanges:userObject:";
+  } else if ([[NSProcessInfo processInfo]
+                 isOperatingSystemAtLeastVersion:iOS_12_2_0]) {
+    methodSignature =
+        (char *)"_elementDidFocus:userIsInteracting:blurPreviousNode:"
+                "changingActivityState:userObject:";
+  }
+
+  if ([[NSProcessInfo processInfo]
+          isOperatingSystemAtLeastVersion:iOS_11_3_0]) {
+    SEL selector = sel_getUid(methodSignature);
+    Method method = class_getInstanceMethod(contentViewClass, selector);
+    IMP original = method_getImplementation(method);
+    IMP override = imp_implementationWithBlock(
+        ^void(id me, void *arg0, BOOL arg1, BOOL arg2, BOOL arg3, id arg4) {
+          ((void (*)(id, SEL, void *, BOOL, BOOL, BOOL, id))original)(
+              me, selector, arg0, TRUE, arg2, arg3, arg4);
+        });
+    method_setImplementation(method, override);
+  } else {
+    SEL selector = sel_getUid(
+        "_startAssistingNode:userIsInteracting:blurPreviousNode:userObject:");
+    Method method = class_getInstanceMethod(contentViewClass, selector);
+    IMP original = method_getImplementation(method);
+    IMP override = imp_implementationWithBlock(
+        ^void(id me, void *arg0, BOOL arg1, BOOL arg2, id arg3) {
+          ((void (*)(id, SEL, void *, BOOL, BOOL, id))original)(
+              me, selector, arg0, TRUE, arg2, arg3);
+        });
+    method_setImplementation(method, override);
+  }
 }
 
 - (void)removeKeyboardInputAccessoryView:(WKWebView*)webview
